fa5661f098af9a7dff7c583c6b431195991004dc
b Null <bnull@offenseindepth.com>
2012-12-17 10:23:44 -0500

    Linux 2.6.35-31 headers

---
+++ 2.6.35-31/include/glib-2.0/gobject/gmarshal.h
@@ -0,0 +1,186 @@
+#ifndef __G_MARSHAL_H__
+#define __G_MARSHAL_H__
+
+G_BEGIN_DECLS
+
+/* VOID:VOID (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:27) */
+extern void g_cclosure_marshal_VOID__VOID (GClosure     *closure,
+                                           GValue       *return_value,
+                                           guint         n_param_values,
+                                           const GValue *param_values,
+                                           gpointer      invocation_hint,
+                                           gpointer      marshal_data);
+
+/* VOID:BOOLEAN (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:28) */
+extern void g_cclosure_marshal_VOID__BOOLEAN (GClosure     *closure,
+                                              GValue       *return_value,
+                                              guint         n_param_values,
+                                              const GValue *param_values,
+                                              gpointer      invocation_hint,
+                                              gpointer      marshal_data);
+
+/* VOID:CHAR (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:29) */
+extern void g_cclosure_marshal_VOID__CHAR (GClosure     *closure,
+                                           GValue       *return_value,
+                                           guint         n_param_values,
+                                           const GValue *param_values,
+                                           gpointer      invocation_hint,
+                                           gpointer      marshal_data);
+
+/* VOID:UCHAR (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:30) */
+extern void g_cclosure_marshal_VOID__UCHAR (GClosure     *closure,
+                                            GValue       *return_value,
+                                            guint         n_param_values,
+                                            const GValue *param_values,
+                                            gpointer      invocation_hint,
+                                            gpointer      marshal_data);
+
+/* VOID:INT (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:31) */
+extern void g_cclosure_marshal_VOID__INT (GClosure     *closure,
+                                          GValue       *return_value,
+                                          guint         n_param_values,
+                                          const GValue *param_values,
+                                          gpointer      invocation_hint,
+                                          gpointer      marshal_data);
+
+/* VOID:UINT (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:32) */
+extern void g_cclosure_marshal_VOID__UINT (GClosure     *closure,
+                                           GValue       *return_value,
+                                           guint         n_param_values,
+                                           const GValue *param_values,
+                                           gpointer      invocation_hint,
+                                           gpointer      marshal_data);
+
+/* VOID:LONG (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:33) */
+extern void g_cclosure_marshal_VOID__LONG (GClosure     *closure,
+                                           GValue       *return_value,
+                                           guint         n_param_values,
+                                           const GValue *param_values,
+                                           gpointer      invocation_hint,
+                                           gpointer      marshal_data);
+
+/* VOID:ULONG (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:34) */
+extern void g_cclosure_marshal_VOID__ULONG (GClosure     *closure,
+                                            GValue       *return_value,
+                                            guint         n_param_values,
+                                            const GValue *param_values,
+                                            gpointer      invocation_hint,
+                                            gpointer      marshal_data);
+
+/* VOID:ENUM (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:35) */
+extern void g_cclosure_marshal_VOID__ENUM (GClosure     *closure,
+                                           GValue       *return_value,
+                                           guint         n_param_values,
+                                           const GValue *param_values,
+                                           gpointer      invocation_hint,
+                                           gpointer      marshal_data);
+
+/* VOID:FLAGS (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:36) */
+extern void g_cclosure_marshal_VOID__FLAGS (GClosure     *closure,
+                                            GValue       *return_value,
+                                            guint         n_param_values,
+                                            const GValue *param_values,
+                                            gpointer      invocation_hint,
+                                            gpointer      marshal_data);
+
+/* VOID:FLOAT (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:37) */
+extern void g_cclosure_marshal_VOID__FLOAT (GClosure     *closure,
+                                            GValue       *return_value,
+                                            guint         n_param_values,
+                                            const GValue *param_values,
+                                            gpointer      invocation_hint,
+                                            gpointer      marshal_data);
+
+/* VOID:DOUBLE (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:38) */
+extern void g_cclosure_marshal_VOID__DOUBLE (GClosure     *closure,
+                                             GValue       *return_value,
+                                             guint         n_param_values,
+                                             const GValue *param_values,
+                                             gpointer      invocation_hint,
+                                             gpointer      marshal_data);
+
+/* VOID:STRING (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:39) */
+extern void g_cclosure_marshal_VOID__STRING (GClosure     *closure,
+                                             GValue       *return_value,
+                                             guint         n_param_values,
+                                             const GValue *param_values,
+                                             gpointer      invocation_hint,
+                                             gpointer      marshal_data);
+
+/* VOID:PARAM (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:40) */
+extern void g_cclosure_marshal_VOID__PARAM (GClosure     *closure,
+                                            GValue       *return_value,
+                                            guint         n_param_values,
+                                            const GValue *param_values,
+                                            gpointer      invocation_hint,
+                                            gpointer      marshal_data);
+
+/* VOID:BOXED (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:41) */
+extern void g_cclosure_marshal_VOID__BOXED (GClosure     *closure,
+                                            GValue       *return_value,
+                                            guint         n_param_values,
+                                            const GValue *param_values,
+                                            gpointer      invocation_hint,
+                                            gpointer      marshal_data);
+
+/* VOID:POINTER (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:42) */
+extern void g_cclosure_marshal_VOID__POINTER (GClosure     *closure,
+                                              GValue       *return_value,
+                                              guint         n_param_values,
+                                              const GValue *param_values,
+                                              gpointer      invocation_hint,
+                                              gpointer      marshal_data);
+
+/* VOID:OBJECT (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:43) */
+extern void g_cclosure_marshal_VOID__OBJECT (GClosure     *closure,
+                                             GValue       *return_value,
+                                             guint         n_param_values,
+                                             const GValue *param_values,
+                                             gpointer      invocation_hint,
+                                             gpointer      marshal_data);
+
+/* VOID:VARIANT (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:44) */
+extern void g_cclosure_marshal_VOID__VARIANT (GClosure     *closure,
+                                              GValue       *return_value,
+                                              guint         n_param_values,
+                                              const GValue *param_values,
+                                              gpointer      invocation_hint,
+                                              gpointer      marshal_data);
+
+/* VOID:UINT,POINTER (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:47) */
+extern void g_cclosure_marshal_VOID__UINT_POINTER (GClosure     *closure,
+                                                   GValue       *return_value,
+                                                   guint         n_param_values,
+                                                   const GValue *param_values,
+                                                   gpointer      invocation_hint,
+                                                   gpointer      marshal_data);
+
+/* BOOL:FLAGS (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:48) */
+extern void g_cclosure_marshal_BOOLEAN__FLAGS (GClosure     *closure,
+                                               GValue       *return_value,
+                                               guint         n_param_values,
+                                               const GValue *param_values,
+                                               gpointer      invocation_hint,
+                                               gpointer      marshal_data);
+#define g_cclosure_marshal_BOOL__FLAGS	g_cclosure_marshal_BOOLEAN__FLAGS
+
+/* STRING:OBJECT,POINTER (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:49) */
+extern void g_cclosure_marshal_STRING__OBJECT_POINTER (GClosure     *closure,
+                                                       GValue       *return_value,
+                                                       guint         n_param_values,
+                                                       const GValue *param_values,
+                                                       gpointer      invocation_hint,
+                                                       gpointer      marshal_data);
+
+/* BOOL:BOXED,BOXED (/build/buildd/glib2.0-2.26.1/gobject/gmarshal.list:50) */
+extern void g_cclosure_marshal_BOOLEAN__BOXED_BOXED (GClosure     *closure,
+                                                     GValue       *return_value,
+                                                     guint         n_param_values,
+                                                     const GValue *param_values,
+                                                     gpointer      invocation_hint,
+                                                     gpointer      marshal_data);
+#define g_cclosure_marshal_BOOL__BOXED_BOXED	g_cclosure_marshal_BOOLEAN__BOXED_BOXED
+
+G_END_DECLS
+
+#endif /* __G_MARSHAL_H__ */
